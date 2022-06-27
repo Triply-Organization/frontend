334aa53bc6fed27bfb8bd9a1b48a218fcdfb2542
pumk <dangkhoa.duong@nfq.asia>
2022-06-27 14:43:22 +0700

    vendor: config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: false,
+      jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
