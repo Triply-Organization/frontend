c82ed0dd9342ba63120dbd3bcc8058788b908b7e
Duong Dang Khoa <104960618+pum-khoa@users.noreply.github.com>
2022-06-27 14:58:53 +0700

    Vendor: install package for project (#1)
    * vendor: install pre-commit Husky, Eslint, Prettier
    
    * vendor: install redux-toolkit
    
    * vendor: set jsx eslint to false
    
    * vendor: install antd
    
    * vendor: config eslint
    
    * vendor: install react-icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react';
+
+import './App.css';
+import logo from './logo.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -11,7 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
         </p>
@@ -39,7 +40,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
