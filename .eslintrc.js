8a2460fb9405dbb2020149cac1796aade0017797
pumk <dangkhoa.duong@nfq.asia>
2022-06-27 14:32:05 +0700

    vendor: set jsx eslint to false

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: false,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
