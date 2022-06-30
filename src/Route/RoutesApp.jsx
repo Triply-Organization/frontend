f15c6ecdbc3847e711930936c63c27f36c79a8ad
pumk <dangkhoa.duong@nfq.asia>
2022-06-30 13:44:18 +0700

    feat: add my tour page

--- src/Route/RoutesApp.jsx
+++ src/Route/RoutesApp.jsx
@@ -6,6 +6,7 @@ import Header from '../components/Header/Header';
 import AllTours from '../pages/Alltours/AllTours';
 import Home from '../pages/Home/Home';
 import Login from '../pages/Login/Login';
+import MyTour from '../pages/MyTour/MyTour';
 import Register from '../pages/Register/Register';
 
 const RoutesApp = () => {
@@ -24,6 +25,7 @@ const RoutesApp = () => {
         <Route index element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/tours" element={<AllTours />} />
+        <Route path="/my-tour" element={<MyTour />} />
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
