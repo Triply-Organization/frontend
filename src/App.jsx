59bc6f2ca026f4e07ef9eae309cbcfc1ed0c0eee
Dang Nguyen Phu Nguyen <104899842+nguyendangnfq@users.noreply.github.com>
2022-06-30 09:44:51 +0700

    Feature/authentication (#16)
    * vendor: install pre-commit Husky, Eslint, Prettier
    
    * vendor: install redux-toolkit
    
    * vendor: set jsx eslint to false
    
    * vendor: install antd
    
    * vendor: config eslint
    
    * vendor: install react-icon
    
    * vendor: install sass
    
    * feat: UI of login page and half of register page
    
    * feat: add axios pakage and config slice redux toolkit
    
    * Vendor: install package for project (#1) (#3)
    
    * Vendor: install package for project (#1)
    
    * vendor: install pre-commit Husky, Eslint, Prettier
    
    * vendor: install redux-toolkit
    
    * vendor: set jsx eslint to false
    
    * vendor: install antd
    
    * vendor: config eslint
    
    * vendor: install react-icon
    
    * Vendor: install sass (#5)
    
    * vendor: install pre-commit Husky, Eslint, Prettier
    
    * vendor: install redux-toolkit
    
    * vendor: set jsx eslint to false
    
    * vendor: install antd
    
    * vendor: config eslint
    
    * vendor: install react-icon
    
    * vendor: install sass
    
    Co-authored-by: Duong Dang Khoa <104960618+pum-khoa@users.noreply.github.com>
    
    * feat: done UI of login page and fixing UI of register page
    
    * fix: button label at register page
    
    * feat: done login feature
    
    * feat: add API to login
    
    * feat: done UI register
    
    * feat: done authentcation feature
    
    * fix: selector border radius
    
    * feat: more valid password validate
    
    Co-authored-by: pumk <dangkhoa.duong@nfq.asia>
    Co-authored-by: nguyendangnfq <nguyendang.nfq.asia>
    Co-authored-by: Quoc Duy <104899197+duyvodev@users.noreply.github.com>
    Co-authored-by: Duong Dang Khoa <104960618+pum-khoa@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import './App.css';
-import Home from './pages/Home/Home';
+import './App.scss';
+import RoutesApp from './Route/RoutesApp';
+import { store } from './app/store';
 
 function App() {
-  return <Home />;
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <RoutesApp />
+        </div>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
