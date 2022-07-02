5fc41cd50fc8e22187f00fdff9818e8f3ccf684b
duyvodev <duy.vo@nfq.asia>
2022-07-02 20:01:22 +0700

    fix: create edit and add modal for users and customers

--- src/pages/Admin/Users/Users.jsx
+++ src/pages/Admin/Users/Users.jsx
@@ -1,68 +1,17 @@
 import { UserOutlined } from '@ant-design/icons';
-import { Avatar, Breadcrumb, Button, Space, Table } from 'antd';
+import { Avatar, Breadcrumb, Button, Form, Input, Space, Table } from 'antd';
 import { Typography } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 
+import ModalForm from '../../../components/ModalForm/ModalForm';
 import './Users.scss';
 
 const { Title } = Typography;
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    // sorter: (a, b) => a.title - b.title,
-  },
-  {
-    title: 'Avatar',
-    dataIndex: 'avatar',
-    key: 'avatar',
-    render: avt => {
-      return (
-        <Avatar
-          src={avt ? avt : null}
-          size="large"
-          icon={!avt ? <UserOutlined /> : null}
-        />
-      );
-    },
-    // sorter: (a, b) => a.customer.length - b.customer.length,
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
-    // sorter: (a, b) => a.duration - b.duration,
-  },
-  {
-    title: 'Phone',
-    dataIndex: 'phone',
-    key: 'phone',
-    // sorter: (a, b) => a.maxPeople - b.maxPeople,
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-    // sorter: (a, b) => a.minAge - b.minAge,
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: () => {
-      return (
-        <Space size={'middle'}>
-          <Button type="primary">Edit</Button>
-          <Button>Delete</Button>
-        </Space>
-      );
-    },
-  },
-];
 const data = [
   {
     name: 'a',
+    key: '1',
     avatar:
       'https://th.bing.com/th/id/OIP.ieXmGxEGTcqBcPcmthIaBgHaEW?pid=ImgDet&rs=1',
     email: '123@gmail.com',
@@ -71,6 +20,7 @@ const data = [
   },
   {
     name: 'b',
+    key: '2',
     avatar: null,
     email: '542@gmail.com',
     phone: '012438123',
@@ -78,6 +28,7 @@ const data = [
   },
   {
     name: 'c',
+    key: '3',
     avatar:
       'https://th.bing.com/th/id/OIP.ieXmGxEGTcqBcPcmthIaBgHaEW?pid=ImgDet&rs=1',
     email: '55555@gmail.com',
@@ -87,8 +38,194 @@ const data = [
 ];
 
 export default function Users() {
+  const [isShowModalAdd, setShowModalAdd] = useState(false);
+  const [isShowModalEdit, setShowModalEdit] = useState(false);
+  const formAddUser = Form.useFormInstance();
+  const formEditUser = Form.useFormInstance();
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      // sorter: (a, b) => a.title - b.title,
+    },
+    {
+      title: 'Avatar',
+      dataIndex: 'avatar',
+      key: 'avatar',
+      render: avt => {
+        return (
+          <Avatar
+            src={avt ? avt : null}
+            size="large"
+            icon={!avt ? <UserOutlined /> : null}
+          />
+        );
+      },
+      // sorter: (a, b) => a.customer.length - b.customer.length,
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      // sorter: (a, b) => a.duration - b.duration,
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone',
+      key: 'phone',
+      // sorter: (a, b) => a.maxPeople - b.maxPeople,
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      // sorter: (a, b) => a.minAge - b.minAge,
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => {
+        return (
+          <Space size={'middle'}>
+            <Button type="primary" onClick={() => setShowModalEdit(true)}>
+              Edit
+            </Button>
+            <Button>Delete</Button>
+          </Space>
+        );
+      },
+    },
+  ];
+
   return (
-    <>
+    <div className="user__wrapper">
+      <ModalForm
+        form={formAddUser}
+        modalTitle="Add new user"
+        formName="add-new-user"
+        isVisible={isShowModalAdd}
+        handleOk={() => console.log(123)}
+        handleCancel={() => setShowModalAdd(false)}
+        okText="Add"
+        // defaultValue={productToEdit}
+      >
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the user name!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="avatar" label="Avatar">
+          <Input type="file" />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the user email!',
+            },
+          ]}
+        >
+          <Input type="email" />
+        </Form.Item>
+        <Form.Item
+          name="phone"
+          label="Phone"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the user phone!',
+            },
+          ]}
+        >
+          <Input type="tel" />
+        </Form.Item>
+        <Form.Item
+          name="address"
+          label="Address"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the user address!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </ModalForm>
+      {/* MODAL EDIT */}
+      <ModalForm
+        form={formEditUser}
+        modalTitle="Edit user"
+        formName="edit-user"
+        isVisible={isShowModalEdit}
+        handleOk={() => console.log(123)}
+        handleCancel={() => setShowModalEdit(false)}
+        okText="Add"
+        // defaultValue={productToEdit}
+      >
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the user name!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="avatar" label="Avatar">
+          <Input type="file" />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the user email!',
+            },
+          ]}
+        >
+          <Input type="email" />
+        </Form.Item>
+        <Form.Item
+          name="phone"
+          label="Phone"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the user phone!',
+            },
+          ]}
+        >
+          <Input type="tel" />
+        </Form.Item>
+        <Form.Item
+          name="address"
+          label="Address"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the user address!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </ModalForm>
       <Breadcrumb
         style={{
           margin: '16px 0',
@@ -112,12 +249,16 @@ export default function Users() {
             display: 'flex',
           }}
         >
-          <Button type="primary" size="large">
+          <Button
+            type="primary"
+            size="large"
+            onClick={() => setShowModalAdd(true)}
+          >
             Add new user
           </Button>
           <Table columns={columns} dataSource={data} />
         </Space>
       </div>
-    </>
+    </div>
   );
 }
