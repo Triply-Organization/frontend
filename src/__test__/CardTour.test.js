bea7c536ea1b63262ac86416f416754987da87a2
duyvodev <duy.vo@nfq.asia>
2022-07-06 09:49:44 +0700

    block

--- src/__test__/CardTour.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import CardTour from '../components/CardTour/CardTour';
-
-it('should render without crashing', () => {
-  ReactDOM.render(<CardTour />, document.createElement('div'));
-});
