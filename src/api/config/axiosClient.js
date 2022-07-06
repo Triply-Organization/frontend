e165d43170bb522c90edaddf2658e9c8113d8b56
pum-khoa <dangkhoa.duong@nfq.asia>
2022-07-07 00:00:53 +0700

    config new api triply.aisa

--- src/api/config/axiosClient.js
+++ src/api/config/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: 'https://api.nhivo-rentcar.me/api/',
+  baseURL: 'https://api.triply.asia/api/',
   headers: {
     'content-type': 'application/json',
   },
