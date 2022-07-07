f1e69c4bd9ed66cb0524dda97819fb1f4909afec
Duong Dang Khoa <104960618+pum-khoa@users.noreply.github.com>
2022-07-07 09:29:49 +0700

    set dotenv server base url (#57)

--- src/api/config/axiosClient.js
+++ src/api/config/axiosClient.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: 'https://api.triply.asia/api/',
+  baseURL: import.meta.env.VITE_SERVER_BASE_URL,
   headers: {
     'content-type': 'application/json',
   },
