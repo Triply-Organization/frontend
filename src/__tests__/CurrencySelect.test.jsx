feea694ee9b66f30b0d44a64ab0d5d3a05daf2fb
duyvodev <duy.vo@nfq.asia>
2022-07-09 21:22:19 +0700

    add components tests

--- src/__tests__/CurrencySelect.test.jsx
+++ src/__tests__/CurrencySelect.test.jsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { describe, expect, test } from 'vitest';
