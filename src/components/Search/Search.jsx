ccd116037b90ccdcac9753bd61df20c873eee8d2
Duong Dang Khoa <104960618+pum-khoa@users.noreply.github.com>
2022-07-10 18:25:20 +0700

    Hotfix/production-v.2.0 (#93)
    * hotfix#5
    
    * hotfix[production-v.2.0]: single select service by id

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -59,7 +59,7 @@ const Search = props => {
         <div>
           <b>{t('search.service.title')}</b>
           <Form.Item
-            name="services[]"
+            name="service"
             className="search-form__item search-form__activity"
           >
             <Select
@@ -69,8 +69,6 @@ const Search = props => {
               showSearch={false}
               data-testid="select-search-services"
               placeholder={t('search.service.place_holder')}
-              mode="multiple"
-              maxTagCount={1}
               style={{ border: 'none' }}
             >
               <OptGroup label="All Activity">
