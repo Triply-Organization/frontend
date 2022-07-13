a05f7a0d3212f0f520bafcf46f0b0e3fb53020c0
pumk <dangkhoa.duong@nfq.asia>
2022-07-13 16:00:22 +0700

    fix: cors 301

--- src/api/checkoutAPI.js
+++ src/api/checkoutAPI.js
@@ -2,17 +2,17 @@ import { axiosClient } from './config/axiosClient';
 
 export const checkoutAPI = {
   checkout(params) {
-    const url = `/checkout/`;
+    const url = `checkout`;
     return axiosClient.post(url, params);
   },
 
   getVoucherInfo(params) {
-    const url = `/vouchers/getinfo`;
+    const url = `vouchers/getinfo`;
     return axiosClient.post(url, params);
   },
 
   getConfirmInfo(params) {
-    const url = `/orders/${decodeURIComponent(params)}`;
+    const url = `orders/${decodeURIComponent(params)}`;
     return axiosClient.get(url, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`,
